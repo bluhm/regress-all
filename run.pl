a7b5b5dc26d5f4af71080ef847b39028ed824f37
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-07-13 18:31:25 +0200

    Add a script the runs the regression tests on a remote machine.

---
+++ run.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Basename;
+use Getopt::Std;
+use POSIX;
+
+my %opts;
+getopts('h:', \%opts) or do {
+    print STDERR "usage: $0 [-h host]\n";
+    exit(2);
+};
+
+# create directory for this test run with timestamp 2016-07-13T12:30:42Z
+my $date = strftime("%FT%TZ", gmtime);
+
+my $dir = dirname($0). "/results";
+-d $dir || mkdir $dir
+    or die "Make result directory '$dir' failed: $!";
+$dir .= "/$date";
+mkdir $dir
+    or die "Make directory '$dir' failed: $!";
+
+# setup remote machines
+
+# run regress there
+
+my @sshcmd = ('ssh', $opts{h}, 'perl', '/root/github/regress-all/regress.pl',
+    '-v', '-e/root/bin/ot-regress');
+system(@sshcmd)
+    and die "Command '@sshcmd' failed: $?";
+my @scpcmd = ('scp', "$opts{h}:/root/github/regress-all/tests.result", $dir);
+system(@scpcmd)
+    and die "Command '@scpcmd' failed: $?";
+
+# display result as html
