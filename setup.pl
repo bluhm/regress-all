493cd7df78a4d72ed6b94b3d156318be2aba392f
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-07-30 01:59:40 +0200

    Run ssh checkout in correct subdir.

--- setup.pl
+++ setup.pl
@@ -70,7 +70,7 @@ system(@scpcmd)
 my ($quiet, $noout) = ("", "");
 $quiet = "-q" unless $opts{v};
 $noout = ">/dev/null" unless $opts{v};
-@sshcmd = ('ssh', $opts{h}, 'sh', '-c',
+@sshcmd = ('ssh', $opts{h},
     "cd /usr && cvs $quiet -R -d /mount/openbsd/cvs co src/regress $noout");
 system(@sshcmd)
     and die "Command '@sshcmd' failed: $?";
