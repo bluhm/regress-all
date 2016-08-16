1158c91f1b9099d905ab3d070a6c705c717812bd
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-08-16 19:13:26 +0200

    Move log functions into module.

---
+++ Logcmd.pm
@@ -0,0 +1,65 @@
+# run commands and log their output
+
+use strict;
+use warnings;
+
+package Logcmd;
+require Exporter;
+our @EXPORT= qw(createlog logmsg runcmd logcmd);
+use subs qw(logmsg);
+
+my ($fh, $file, $verbose);
+sub createlog (%) {
+    my %args = @_;
+    $file = $args{file};
+    open(my $fh, '>', $file)
+	or die "Open '$file' for writing failed: $!";
+    $fh->autoflush();
+    $verbose = $args{verbose};
+    $| = 1 if $verbose;
+}
+
+$SIG{__DIE__} = sub {
+    print $fh @_;
+    die @_;
+};
+
+sub logmsg (@) {
+    print $fh @_;
+    print @_ if $verbose;
+}
+
+sub runcmd (@) {
+    my @cmd = @_;
+    logmsg "Command '@cmd' started\n";
+    system(@cmd)
+	and die "Command '@cmd' failed: $?";
+    logmsg "Command '@cmd' finished\n";
+}
+
+sub logcmd (@) {
+    my @cmd = @_;
+    logmsg "Command '@cmd' started\n";
+    defined(my $pid = open(my $out, '-|'))
+	or die "Open pipe from '@cmd' failed: $!";
+    if ($pid == 0) {
+	close($out);
+	open(STDIN, '<', "/dev/null")
+	    or warn "Redirect stdin to /dev/null failed: $!";
+	open(STDERR, '>&', \*STDOUT)
+	    or warn "Redirect stderr to stdout failed: $!";
+	setsid()
+	    or warn "Setsid $$ failed: $!";
+	exec(@cmd);
+	warn "Exec '@cmd' failed: $!";
+	_exit(126);
+    }
+    while (<$out>) {
+	s/[^\s[:print:]]/_/g;
+	logmsg $_;
+    }
+    close($out) or die $! ?
+	"Close pipe from '@cmd' failed: $!" :
+	"Command '@cmd' failed: $?";
+    logmsg "Command '@cmd' finished\n";
+}
