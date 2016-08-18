0fbab95a1d91f33f19c626b8bd7d9452c2e4dff3
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-08-18 13:38:30 +0200

    Use Carp in Logcmd module.

--- Logcmd.pm
+++ Logcmd.pm
@@ -4,6 +4,7 @@ package Logcmd;
 
 use strict;
 use warnings;
+use Carp;
 
 use parent 'Exporter';
 our @EXPORT= qw(createlog logmsg runcmd logcmd);
@@ -14,7 +15,7 @@ sub createlog (%) {
     my %args = @_;
     $file = $args{file};
     open($fh, '>', $file)
-	or die "Open '$file' for writing failed: $!";
+	or croak "Open '$file' for writing failed: $!";
     $fh->autoflush();
     $verbose = $args{verbose};
     $| = 1 if $verbose;
@@ -34,7 +35,7 @@ sub runcmd (@) {
     my @cmd = @_;
     logmsg "Command '@cmd' started\n";
     system(@cmd)
-	and die "Command '@cmd' failed: $?";
+	and croak "Command '@cmd' failed: $?";
     logmsg "Command '@cmd' finished\n";
 }
 
@@ -42,24 +43,27 @@ sub logcmd (@) {
     my @cmd = @_;
     logmsg "Command '@cmd' started\n";
     defined(my $pid = open(my $out, '-|'))
-	or die "Open pipe from '@cmd' failed: $!";
+	or croak "Open pipe from '@cmd' failed: $!";
     if ($pid == 0) {
 	close($out);
 	open(STDIN, '<', "/dev/null")
-	    or warn "Redirect stdin to /dev/null failed: $!";
+	    or carp "Redirect stdin to /dev/null failed: $!";
 	open(STDERR, '>&', \*STDOUT)
-	    or warn "Redirect stderr to stdout failed: $!";
+	    or carp "Redirect stderr to stdout failed: $!";
 	setsid()
-	    or warn "Setsid $$ failed: $!";
-	exec(@cmd);
-	warn "Exec '@cmd' failed: $!";
+	    or carp "Setsid $$ failed: $!";
+	{
+	    no warnings 'exec';
+	    exec(@cmd);
+	    carp "Exec '@cmd' failed: $!";
+	}
 	_exit(126);
     }
     while (<$out>) {
 	s/[^\s[:print:]]/_/g;
 	logmsg $_;
     }
-    close($out) or die $! ?
+    close($out) or croak $! ?
 	"Close pipe from '@cmd' failed: $!" :
 	"Command '@cmd' failed: $?";
     logmsg "Command '@cmd' finished\n";
