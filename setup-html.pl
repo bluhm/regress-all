01118aa6d3cfce7ffbf00babec2010f289215581
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-08-19 15:48:15 +0200

    Run over all direcotries to create setup html.

--- setup-html.pl
+++ setup-html.pl
@@ -17,81 +17,89 @@ getopts('d:', \%opts) or do {
     print STDERR "usage: $0 -d date\n";
     exit(2);
 };
-my $date = $opts{d} or die "No -d specified";
 
 my $dir = dirname($0). "/..";
 chdir($dir)
     or die "Chdir to '$dir' failed: $!";
 my $regressdir = getcwd();
-$dir = "results/$date";
+$dir = "results";
 chdir($dir)
     or die "Chdir to '$dir' failed: $!";
 
-my %h;
-foreach my $version (glob("version-*.txt")) {
-    my ($host) = $version =~ m,version-(.*)\.txt,;
-    open(my $fh, '<', $version)
-	or die "Open '$version' for reading failed: $!";
-    defined(my $line = <$fh>)
-	or next;
-    my ($time, $short) = $line =~ m,: ((\w+ \w+ \d+) .*)$,;
-    $h{$host} = {
-	version => $version,
-	time => $time,
-	short => $short,
-    };
-}
-foreach my $setup (glob("setup-*.log")) {
-    my ($host) = $setup =~ m,setup-(.*)\.log,;
-    $h{$host}{setup} = $setup,
-}
+my @dates = $opts{d} || map { dirname($_) } glob("*/run.log");
+my %d;
+foreach my $date (@dates) {
+    $dir = "$regressdir/results/$date";
+    chdir($dir)
+	or die "Chdir to '$dir' failed: $!";
 
-open(my $html, '>', "setup.html")
-    or die "Open 'setup.html' for writing failed: $!";
-print $html "<!DOCTYPE html>\n";
-print $html "<html>\n";
-print $html "<head>\n";
-print $html "  <title>OpenBSD Test Setup</title>\n";
-print $html "</head>\n";
-
-print $html "<body>\n";
-print $html "<h1>OpenBSD regress test machine</h1>\n";
-print $html "<table>\n";
-print $html "  <tr>\n    <th>setup at</th>\n";
-print $html "    <td>$now</td>\n";
-print $html "  </tr>\n";
-print $html "  <tr>\n    <th>test at</th>\n";
-print $html "    <td>$date</td>\n";
-print $html "  </tr>\n";
-print $html "</table>\n";
-print $html "<table>\n";
-print $html "  <tr>\n    <th>machine</th>\n";
-print $html "    <th>version</th>\n";
-print $html "    <th>setup</th>\n";
-print $html "  </tr>\n";
-
-foreach my $host (sort keys %h) {
-    print $html "  <tr>\n    <th>$host</th>\n";
-    my $version = uri_escape($h{$host}{version});
-    my $time = encode_entities($h{$host}{time});
-    my $short = $h{$host}{short};
-    my $setup = uri_escape($h{$host}{setup});
-    if ($version) {
-	print $html "    <td title=\"$time\">".
-	    "<a href=\"$version\">$short</a></td>\n";
-    } else {
-	print $html "    <td></td>";
+    my %h;
+    foreach my $version (glob("version-*.txt")) {
+	my ($host) = $version =~ m,version-(.*)\.txt,;
+	open(my $fh, '<', $version)
+	    or die "Open '$version' for reading failed: $!";
+	defined(my $line = <$fh>)
+	    or next;
+	my ($time, $short) = $line =~ m,: ((\w+ \w+ \d+) .*)$,;
+	$h{$host} = {
+	    version => $version,
+	    time => $time,
+	    short => $short,
+	};
     }
-    if ($setup) {
-	print $html "    <td><a href=\"$setup\">log</a></td>\n";
-    } else {
-	print $html "    <td></td>";
+    foreach my $setup (glob("setup-*.log")) {
+	my ($host) = $setup =~ m,setup-(.*)\.log,;
+	$h{$host}{setup} = $setup,
     }
+    $d{$date}{host} = \%h;
+
+    open(my $html, '>', "setup.html")
+	or die "Open 'setup.html' for writing failed: $!";
+    print $html "<!DOCTYPE html>\n";
+    print $html "<html>\n";
+    print $html "<head>\n";
+    print $html "  <title>OpenBSD Test Setup</title>\n";
+    print $html "</head>\n";
+
+    print $html "<body>\n";
+    print $html "<h1>OpenBSD regress test machine</h1>\n";
+    print $html "<table>\n";
+    print $html "  <tr>\n    <th>setup at</th>\n";
+    print $html "    <td>$now</td>\n";
     print $html "  </tr>\n";
-}
-print $html "</table>\n";
-print $html "</body>\n";
+    print $html "  <tr>\n    <th>test at</th>\n";
+    print $html "    <td>$date</td>\n";
+    print $html "  </tr>\n";
+    print $html "</table>\n";
+    print $html "<table>\n";
+    print $html "  <tr>\n    <th>machine</th>\n";
+    print $html "    <th>version</th>\n";
+    print $html "    <th>setup</th>\n";
+    print $html "  </tr>\n";
 
-print $html "</html>\n";
-close($html)
-    or die "Close 'setup.html' after writing failed: $!";
+    foreach my $host (sort keys %h) {
+	print $html "  <tr>\n    <th>$host</th>\n";
+	my $version = uri_escape($h{$host}{version});
+	my $time = encode_entities($h{$host}{time});
+	my $short = $h{$host}{short};
+	my $setup = uri_escape($h{$host}{setup});
+	if ($version) {
+	    print $html "    <td title=\"$time\">".
+		"<a href=\"$version\">$short</a></td>\n";
+	} else {
+	    print $html "    <td></td>";
+	}
+	if ($setup) {
+	    print $html "    <td><a href=\"$setup\">log</a></td>\n";
+	} else {
+	    print $html "    <td></td>";
+	}
+	print $html "  </tr>\n";
+    }
+    print $html "</table>\n";
+    print $html "</body>\n";
+
+    print $html "</html>\n";
+    close($html)
+	or die "Close 'setup.html' after writing failed: $!";
+}
