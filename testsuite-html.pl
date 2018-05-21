d68dbf22f85e76dfc496c2427410e3d51991476f
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-05-21 13:32:14 +0200

    Adding hostname to out directory avoids overwrite.

--- testsuite-html.pl
+++ testsuite-html.pl
@@ -102,8 +102,8 @@ while (my ($host, $date) = each %latesthost) {
 	"-s,.*\.core\$,,",
 	"-s,.*\.c\$,,",
 	"-s,.*\.sh\$,,",
-	"-s,^/misc/os-test/,$publish/os-test/out/$snap/,",
-	"-s,^/misc/posixtestsuite/,$publish/posixtestsuite/out/$snap/,",
+	"-s,^/misc/os-test/,$publish/os-test/out/$host-$snap/,",
+	"-s,^/misc/posixtestsuite/,$publish/posixtestsuite/out/$host-$snap/,",
 	"-s,.*,,");
     system(@pax)
 	and die "Command '@pax' failed: $?";
