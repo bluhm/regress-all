f6d8c3318749f4201750cfd8a620621feaa1a357
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-01-31 20:11:46 +0100

    Bump copyright year.

--- setup-html.pl
+++ setup-html.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # convert test setup details to a html table
 
-# Copyright (c) 2016-2017 Alexander Bluhm <bluhm@genua.de>
+# Copyright (c) 2016-2019 Alexander Bluhm <bluhm@genua.de>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
