ff351ab6df3c6e84269073a00abbeebbc851c64c
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-07-19 17:54:36 +0200

    Script makealign takes ld.script and adds align per object to stdout.

---
+++ makealign.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -
+# adapt kernel linker script to align all objects to page size
+
+if [[ $# < 7 ]]; then
+	echo usage: makealign.sh ld.script \
+	    locore0.o gap.o ... param.o ioconf.o vers.o 2>&1
+	exit 2
+fi
+
+LDSCRIPT=$1
+shift
+OBJS="$@"
+
+PAGE_SIZE=$(sysctl -n hw.pagesize)
+
+# create template
+for f in $OBJS;
+do
+	TEMPLATE=$(cat <<- EOF
+	$TEMPLATE
+	. = ALIGN($PAGE_SIZE);
+	$f(SECTION);
+EOF)
+done
+
+while read l;
+do
+	SECTION=$(echo "$l" | grep -o '\s*\*(\(.*\))$')
+	if [ $? -eq 0 ]; then
+		echo "$TEMPLATE" | \
+		    sed "s/SECTION/$(echo $SECTION| cut -c 2- | tr -d '()')/"
+	else
+		echo "$l"
+	fi
+done <$LDSCRIPT
