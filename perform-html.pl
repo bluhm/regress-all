bb1b254abab7c6886f625ce1d525a215ef74f8d3
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-12-25 18:13:56 +0100

    Hash tree may be created by more than status, skip block.

--- perform-html.pl
+++ perform-html.pl
@@ -167,7 +167,7 @@ foreach my $result (@results) {
 	    };
 	}
 	undef @values;
-	if (($t{$test}{$date}{severity} || 0 ) < $severity) {
+	if ($t{$test}{$date} && $t{$test}{$date}{severity} < $severity) {
 	    $t{$test}{$date}{status} = $status;
 	    $t{$test}{$date}{severity} = $severity;
 	}
@@ -618,7 +618,11 @@ HEADER
 		or next;
 	    print $html "  <tr>\n    <th>$test</th>\n";
 	    foreach my $repeat (@repeats) {
-		my $status = $td->{$repeat} ? $td->{$repeat}{status} : "";
+		unless ($td->{$repeat}) {
+		    print $html "    <td></td>\n";
+		    next;
+		}
+		my $status = $td->{$repeat}{status};
 		my $class = " class=\"result $status\"";
 		my $message = encode_entities($td->{$repeat}{message});
 		my $title = $message ? " title=\"$message\"" : "";
@@ -662,7 +666,11 @@ HEADER
 		    $outlier = $vt->{outlier}[$i] = abs($relative) >= 0.025;
 		}
 		foreach my $repeat (@repeats) {
-		    my $status = $td->{$repeat} ? $td->{$repeat}{status} : "";
+		    unless ($td->{$repeat}) {
+			print $html "    <td></td>\n";
+			next;
+		    }
+		    my $status = $td->{$repeat}{status};
 		    if ($status ne 'PASS') {
 			print $html "    <td></td>\n";
 			next;
@@ -886,7 +894,11 @@ HEADER
 	my $td = $t{$test}{$date} or next;
 	print $html "  <tr>\n    <th>$test</th>\n";
 	foreach my $cvsdate (@cvsdates) {
-	    my $status = $td->{$cvsdate} ? $td->{$cvsdate}{status} : "";
+	    unless ($td->{$cvsdate}) {
+		print $html "    <td></td>\n";
+		next;
+	    }
+	    my $status = $td->{$cvsdate}{status};
 	    my $class = " class=\"result $status\"";
 	    my $message = encode_entities($td->{$cvsdate}{message});
 	    my $title = $message ? " title=\"$message\"" : "";
@@ -944,7 +956,11 @@ HEADER
 		$outlier = $vt->{outlier}[$i] = abs($relative) >= 0.025;
 	    }
 	    foreach my $cvsdate (@cvsdates) {
-		my $status = $td->{$cvsdate} ? $td->{$cvsdate}{status} : "";
+		unless ($td->{$cvsdate}) {
+		    print $html "    <td></td>\n";
+		    next;
+		}
+		my $status = $td->{$cvsdate}{status};
 		if ($status ne 'PASS' && !$rp0) {
 		    print $html "    <td></td>\n";
 		    next;
@@ -1124,7 +1140,12 @@ print $html "  </tr>\n";
 foreach my $test (@tests) {
     print $html "  <tr>\n    <th>$test</th>\n";
     foreach my $date (@dates) {
-	my $status = $t{$test}{$date} ? $t{$test}{$date}{status} : "";
+	my $td = $t{$test}{$date};
+	unless ($td) {
+	    print $html "    <td></td>\n";
+	    next;
+	}
+	my $status = $td->{status};
 	my $class = " class=\"result $status\"";
 	my $message = encode_entities($t{$test}{$date}{message});
 	my $title = $message ? " title=\"$message\"" : "";
