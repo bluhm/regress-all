c4c05f52bfbbfabfb4fecddfca980f5ca3096ee9
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-11-25 21:46:52 +0100

    When stepping through commits, also stop and measure at quirk.

--- step.pl
+++ step.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Copyright (c) 2018-2019 Alexander Bluhm <bluhm@genua.de>
+# Copyright (c) 2018-2020 Alexander Bluhm <bluhm@genua.de>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -24,6 +24,7 @@ use POSIX;
 use Time::Local;
 
 use lib dirname($0);
+use Buildquirks;
 use Logcmd;
 use Hostctl;
 
@@ -161,7 +162,9 @@ setup_html();
 
 my @steps;
 if ($unit eq "commit") {
-    @steps = get_commits($begin, $end);
+    my %times;
+    @times{(get_commits($begin, $end), get_quirks($begin, $end))} = ();
+    @steps = sort keys %times;
     unshift @steps, $begin unless @steps && $steps[0] == $begin;
     push @steps, $end unless $steps[-1] == $end;
 } else {
@@ -175,7 +178,6 @@ if ($unit eq "commit") {
 }
 
 foreach my $current (@steps) {
-
     chdir($performdir)
 	or die "Chdir to '$performdir' failed: $!";
 
@@ -276,6 +278,13 @@ sub get_commits {
 	push @steps, $time;
     }
     return @steps;
+}
+
+sub get_quirks {
+    my ($before, $after) = map { strftime("%FT%TZ", gmtime($_)) } @_;
+
+    my %q = quirks($before, $after);
+    return map { $q{$_}{commit} } sort keys %q;
 }
 
 sub add_step {
