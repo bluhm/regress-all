1730448c236731b44d397d8a22f667065672f0f3
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-12-23 15:23:39 +0100

    Do not use temp file and some stylistic changes in keys.

--- keys.sh
+++ keys.sh
@@ -3,23 +3,25 @@
 set -eu
 
 usage() {
-	echo "$(basename $0): FROM TO" 1>&2
-	echo "\tgenerate pngs of the gnuplot keys in a given range using" 1>&2
-	echo "\timagemagick's convert. FROM and TO are positive integers" 1>&2
+	echo "$(basename $0): FROM TO" >&2
+	echo "\tGenerate pngs of the gnuplot keys in a given range using" >&2
+	echo "\timagemagick's convert. FROM and TO are positive integers." >&2
 	exit 1
 }
 
 FROM="$1"
 TO="$2"
-which gnuplot > /dev/null && which convert > /dev/null || usage
+which gnuplot >/dev/null && which convert >/dev/null || usage
 if [ -z "$FROM" ] || [ -n "${FROM#[0-9]*}" ] || [ "$FROM" -le "0" ] ||\
     [ -z "$TO" ] || [ -n "${TO##[0-9]*}" ] || [ "$TO" -lt "$FROM" ]; then
 	usage
 fi
 
-PLOT=$(mktemp)
-cat > $PLOT << EOG
-\$point << EOD
+PLOT=keys.pl
+trap "rm -f $PLOT $PLOT.new" 0
+
+cat >$PLOT.new <<EOG
+\$point <<EOD
 1
 EOD
 set xrange [0.99:1.01]
@@ -30,8 +32,9 @@ do for [i = $FROM:$TO] {
 	plot \$point using 1:1 with points lc i pt i
 }
 EOG
+mv $PLOT.new $PLOT
 
-gnuplot $PLOT >> /dev/null
+gnuplot $PLOT >/dev/null
 
 for key in key_*.png; do
 	convert $key -crop 16x24+339+218 $key
