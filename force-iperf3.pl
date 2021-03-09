37a51fa9de9114a8d17c13cbb9cca4b3e211a86d
Alexander Bluhm <alexander.bluhm@gmx.net>
2021-03-10 00:20:02 +0100

    Allow to run iperf3 server daemon on lt machines.

--- force-iperf3.pl
+++ force-iperf3.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -T
-# Allow ssh to run any iperf3 client, but only iperf3.
+# Allow ssh to run any iperf3 client or server daemon, but only iperf3.
 
 # Copyright (c) 2021 Alexander Bluhm <bluhm@openbsd.org>
 #
@@ -33,16 +33,19 @@ if (@args == 2 && $args[0] eq "pkill" && $args[1] eq "iperf3") {
 $args[0] eq "iperf3"
     or die "Only 'iperf3' command allowed.\n";
 
-# filter out server and file access options
+# filter out file access options
 my %opts;
 @ARGV = @args;
 shift;
-getopts('p:f:i:A:B:V:J:d:v:h:c:ub:t:n:k:l:P:Rw:M:N46S:L:Z:O:T:C:', \%opts)
+getopts('p:f:i:A:B:V:J:d:v:hsD1c:ub:t:n:k:l:P:Rw:M:N46S:L:Z:O:T:C:', \%opts)
     or die "Parsing iperf3 options failed.\n";
-$opts{c}
-    or die "Client option for iperf3 must be present.\n";
-$opts{c} =~ /^10\.[0-9.]+$/ || $opts{c} =~ /^f[cd][0-9a-f]{2}:[0-9a-f:]+$/i
-    or die "Server address must be local.\n";
+if ($opts{c}) {
+    $opts{c} =~ /^10\.[0-9.]+$/ || $opts{c} =~ /^f[cd][0-9a-f]{2}:[0-9a-f:]+$/i
+	or die "Server address must be local.\n";
+} elsif ($opts{s}) {
+} else {
+    die "Client or server option for iperf3 must be present.\n";
+}
 
 $ENV{PATH} = "/bin:/usr/bin:/usr/local/bin";
 exec { 'iperf3' } @args;
