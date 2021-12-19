a9f846f38ff2b293942e53be4dcb242f19e51b36
Alexander Bluhm <alexander.bluhm@gmx.net>
2021-12-19 23:30:05 +0100

    Quirk for update LLVM to 13.0.0.

--- Buildquirks.pm
+++ Buildquirks.pm
@@ -563,7 +563,27 @@ my %quirks = (
 	comment => "remove dangling crypto noqueue, missing part of commit",
 	updatedirs => [ "sys" ],
 	patches => { 'sys-softraid-crypto' => patch_sys_softraid_crypto() },
-    }
+    },
+    '2021-12-17T14:55:47Z' => {
+	comment => "clang, libc++, and libc++abi update LLVM to 13.0.0",
+	updatedirs => [
+	    "gnu/llvm",
+	    "gnu/usr.bin/clang",
+	    "gnu/lib/libcxx",
+	    "gnu/lib/libcxxabi",
+	],
+	cleandirs => [
+	    "gnu/usr.bin/clang",
+	    "gnu/lib/libcxx",
+	    "gnu/lib/libcxxabi",
+	    "sys/arch/amd64/compile/GENERIC.MP",
+	],
+	builddirs => [
+	    "gnu/usr.bin/clang",
+	    "gnu/lib/libcxx",
+	    "gnu/lib/libcxxabi",
+	],
+    },
 );
 
 #### Patches ####
