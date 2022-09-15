b34e75bf4acd59bb2e13f0fad07d7bafbe67852f
Alexander Bluhm <alexander.bluhm@gmx.net>
2022-09-15 18:44:51 +0200

    Use kstack.sh to create svg files manually.

---
+++ kstack.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+fg=/home/bluhm/github/FlameGraph
+file="${1:-kstack}"
+host="${2:-ot14}"
+
+# btrace -e 'profile:hz:100{@[kstack]=count()}' >~/"$file.dt" &
+# sleep 60; kill -INT $!
+
+ssh "$host" cat "$file.dt" | \
+    $fg/stackcollapse-bpftrace.pl | \
+    $fg/flamegraph.pl > \
+    /data/test/files/"$host-$file.svg"
