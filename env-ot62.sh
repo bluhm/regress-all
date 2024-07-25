a93c4aa7900eb736865c47efdd51892e2388d1c5
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-07-25 22:32:15 +0200

    Add environment and packages for netlink ot62.

---
+++ env-ot62.sh
@@ -0,0 +1,14 @@
+export MANAGEMENT_IF=vio2
+export SKIP_IF=vio2
+export NETLINK_LINE=7
+export LINUX_IF=eth1
+export LINUX_LEFT_SSH=root@lt61
+export LINUX_RIGHT_SSH=root@lt63
+
+export REGRESS_FAIL_EARLY=no
+export TEST_SSH_UNSAFE_PERMISSIONS=yes
+
+LOCAL_IF=vio1
+LOCAL_MAC=fe:e1:bb:d2:dc:a1
+
+export LOCAL_IF LOCAL_MAC
