c456b09c11b25a5b3478bcb17cc039196533c8bb
Alexander Bluhm <alexander.bluhm@gmx.net>
2025-01-10 13:29:42 +0100

    Bump copyright year.

--- netlink-html.pl
+++ netlink-html.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # convert netlink test results to html tables
 
-# Copyright (c) 2016-2024 Alexander Bluhm <bluhm@genua.de>
+# Copyright (c) 2016-2025 Alexander Bluhm <bluhm@genua.de>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
