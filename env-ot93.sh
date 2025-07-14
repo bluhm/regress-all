e2fb5bf010529e75181a1f3e798d618bfde8e126
Alexander Bluhm <alexander.bluhm@gmx.net>
2025-07-14 23:09:43 +0200

    Set correct netlink line for ot93 ot94.

--- env-ot93.sh
+++ env-ot93.sh
@@ -1,9 +1,9 @@
 export MANAGEMENT_IF=vio2
 export SKIP_IF=vio2
-export NETLINK_LINE=0
-export LINUX_IF=enp7s0
-export LINUX_LEFT_SSH=root@lt90
-export LINUX_RIGHT_SSH=root@lt95
+export NETLINK_LINE=3
+export LINUX_IF=enp7s0,enp7s0
+export LINUX_LEFT_SSH=root@lt90,root@lt98
+export LINUX_RIGHT_SSH=root@lt95,root@lt99
 
 export REGRESS_FAIL_EARLY=no
 export TEST_SSH_UNSAFE_PERMISSIONS=yes
