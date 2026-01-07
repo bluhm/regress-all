2ea50d02d61c7bfa1e108ddcc67fde28ab9ea527
Alexander Bluhm <alexander.bluhm@gmx.net>
2026-01-07 21:29:02 +0100

    Retire lt100 lt101 for multiple testing on ot41.

--- env-ot41.sh
+++ env-ot41.sh
@@ -1,10 +1,10 @@
 export MANAGEMENT_IF=em0
 export SKIP_IF=em0,em1
 export NETLINK_LINE=1
-export LINUX_IF=enp1s0f0np0,ens1f0np0
-export LINUX_DIRECT_IF=enp1s0f1np1,ens1f1np1
-export LINUX_LEFT_SSH=root@lt40,root@lt100
-export LINUX_RIGHT_SSH=root@lt43,root@lt101
+export LINUX_IF=enp1s0f0np0
+export LINUX_DIRECT_IF=enp1s0f1np1
+export LINUX_LEFT_SSH=root@lt40
+export LINUX_RIGHT_SSH=root@lt43
 export TREX_LEFT_IF=09:00.0
 export TREX_RIGHT_IF=0a:00.0
 export TREX_SSH=root@lt40
