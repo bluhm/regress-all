01f14f7365740283e44f3e80bdca5a1b5b989eba
Alexander Bluhm <alexander.bluhm@gmx.net>
2026-03-10 16:17:28 +0100

    Netlink networks on vmd machines ot17 ot52 do not have fixed laddr.

--- env-ot69.sh
+++ env-ot69.sh
@@ -7,8 +7,3 @@ export LINUX_RIGHT_SSH=root@lt75
 
 export REGRESS_FAIL_EARLY=no
 export TEST_SSH_UNSAFE_PERMISSIONS=yes
-
-LOCAL_IF=vio1
-LOCAL_MAC=fe:e1:bb:d2:ec:87
-
-export LOCAL_IF LOCAL_MAC
--- env-ot77.sh
+++ env-ot77.sh
@@ -7,8 +7,3 @@ export LINUX_RIGHT_SSH=root@lt75
 
 export REGRESS_FAIL_EARLY=no
 export TEST_SSH_UNSAFE_PERMISSIONS=yes
-
-LOCAL_IF=vio1
-LOCAL_MAC=fe:e1:bb:d2:bc:ae
-
-export LOCAL_IF LOCAL_MAC
